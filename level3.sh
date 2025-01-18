e1b942cd294abad674ff9314825426e2f7019eb7
Abhay Pratap Yadav <codingcontestabhay@gmail.com>
2025-01-18 18:21:17 +0530

    Update level3.sh

--- level3.sh
+++ level3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the flag content
-flag="flag3{m0r3_0bscur1ty_m0r3_t1m3}"
+flag="QS{Crypt0_M4st3r_2024}"
 
 # Create a root directory for the challenge
 mkdir -p level3_ctf
@@ -11,35 +11,33 @@ cd level3_ctf
 mkdir .hidden_area
 cd .hidden_area
 
-# Create a file with an obfuscated flag
-echo "VGhpcyBpcyBzb21lIHRleHQuLi4=" > obfuscated_file1
-echo "SGVyZSBpcyBzb21lIG1vcmUgdGV4dC4uLi4=" >> obfuscated_file1
-echo "$(echo "$flag" | base64 | tr 'a-z' 'n-za-m')" >> obfuscated_file1 # base64 and ROT13
-echo "QW5kIHRoaXMganVzdCBzb21lIG1vcmUgdGV4dA==" >> obfuscated_file1
+# Create a file with an obfuscated flag - only base64 for simplicity
+echo "$(echo "$flag" | base64)" > obfuscated_file1
 
 # Create a decoy file
 touch decoy1.txt
 echo "Try using cat or find here." > decoy1.txt
 
-# Create a directory with a special name and permissions - changed permission
-mkdir --mode=755 special_dir
-cd special_dir
+# Create a directory for the link
+mkdir symlink_dir
+cd symlink_dir
 
-# Create a symbolic link to the obfuscated file - changed relative link
+# Create a symbolic link to the obfuscated file - simpler relative path
 ln -s ../obfuscated_file1 link_to_flag
 
-# Changed script to be in the special dir, also changed the command to be a relative path to the ln
+# Create the decoding script, simple and direct
 echo "#!/bin/bash" > decode_flag.sh
-echo "cat link_to_flag | tr 'n-za-m' 'a-z' | base64 -d" >> decode_flag.sh
+echo "cat link_to_flag | base64 -d" >> decode_flag.sh
 chmod +x decode_flag.sh
 
 cd ..
 cd ..
 
+
 # Create a binary file
 dd if=/dev/urandom of=level3_ctf/binary.bin bs=1M count=2
-echo "Hint: You may want to follow the symlink and use a script to get the flag." > level3_ctf/hint.txt
+echo "Hint: Follow the symlink." > level3_ctf/hint.txt
 
 # Message to the user
 echo "Level 3 Challenge Setup Complete!"
-echo "Good luck. You'll need to take your time and explore carefully."
+echo "This one should be a bit easier now!"
