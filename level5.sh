c9269cfe1edfcd980e7cadeb16b0f8be2b910eca
Abhay Pratap Yadav <codingcontestabhay@gmail.com>
2025-01-18 23:47:06 +0530

    Update level5.sh

--- level5.sh
+++ level5.sh
@@ -7,10 +7,34 @@ flag="FLAG=QS{Qu1ckSn4tch_Ch4mp10n_2024} Massage=like a true"
 mkdir -p level5_ctf
 cd level5_ctf
 
-# Create a file with base64 encoded flag
+# Create a hidden directory
+mkdir .secret_stuff
+cd .secret_stuff
+
+# Create decoy files
+touch decoy1.txt
+echo "This isn't the file you are looking for" > decoy1.txt
+touch decoy2.txt
+echo "Keep searching" > decoy2.txt
+
+
+# Create the directory and flag file
+mkdir data
+cd data
 echo "$(echo "$flag" | base64)" > encoded_flag
 chmod 444 encoded_flag
 
+# Create decoy files in the directory
+touch decoy3.txt
+echo "This might be important" > decoy3.txt
+touch decoy4.txt
+echo "Or maybe not?" > decoy4.txt
+
+cd ..
+cd ..
+# Create a dummy file
+dd if=/dev/urandom of=level5_ctf/dummy.bin bs=1M count=2
+
 # Provide some initial setup info to the user
 echo "Level 5 Challenge Started!"
-echo "The flag is encoded using a common encoding scheme. If you know the encoding, a specific command could assist in decoding it. Check your command line tools."
+echo "You need to explore thoroughly to find the encoded flag. Once you find it you will need to decode it using a specific command."
